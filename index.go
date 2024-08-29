44e7de91ae59406e6e6c2694fe7d88f9108b371a
Joseph Richie <joseph.richie@qumu.com>
2024-08-29 12:21:59 +0530

    Added endpoint to Delete book

--- index.go
+++ index.go
@@ -31,6 +31,7 @@ func main() {
 	request.GET("/getAllFinishedBooks", getAllFinishedBooks)
 	request.GET("/getBooksByAuthor", getBooksByAuthor)
 	request.GET("/getBooksReadInAPeriod", getBooksReadInAPeriod)
+	request.DELETE("/deleteBook", deleteBook)
 	request.Run(":8083")
 
 }
@@ -1149,4 +1150,62 @@ func getBooksReadInAPeriod(c *gin.Context) {
 	// Returning all the data
 	c.JSON(200, gin.H{"booksByAuthor": getBookDetails})
 
+}
+
+// Defining JSON body for deleteBook(). It requires 1 Query Parameter bookID.
+type DeleteBookDetailsParameters struct {
+	BookID string `form:"bookID" binding:"required"`
+}
+
+// Returns a single Book Details
+func deleteBook(c *gin.Context) {
+
+	// Variables for DB and Error
+	var db *sql.DB
+	var err error
+
+	// Creating an instance of the struct, DeleteBookDetailsParameters
+	var deleteBookDetailsParameters DeleteBookDetailsParameters
+
+	// Bind to the struct's members. If any member is invalid, binding does not happen and an error will be returned. Then its rejected with 400
+	if c.Bind(&deleteBookDetailsParameters) != nil {
+		c.JSON(400, gin.H{"status": "Incorrect parameters, please provide all required parameters"})
+		return
+	}
+
+	// Connect to the DB. If there is any issue connecting to the DB, throw a 500 error and return
+	db, err = sql.Open("sqlite", "./BOOKMANAGEMENT.db")
+	if err != nil {
+		c.JSON(500, gin.H{"status": "Could not connect to DB"})
+		return
+	}
+	defer db.Close()
+
+	// Check if the exists in the DB by querying using the ID
+	// Result is scanned into the variable, result
+	queryToCheckExistingBook := `SELECT ID FROM BOOKMANAGEMENT WHERE ID = $1;`
+	result := db.QueryRow(queryToCheckExistingBook, deleteBookDetailsParameters.BookID)
+
+	// Defining a struct to hold all the values from the Query result
+	type GetBookDetails struct {
+		ID string
+	}
+
+	// Creating an instance of the struct, GetBookDetails
+	var getBookDetails GetBookDetails
+
+	// Scan the query result into the struct's members
+	result.Scan(&getBookDetails.ID)
+
+	// If the length of getBookDetails.ID is greater than 0, means the query returned a result, so there is a book by that ID
+	// We delete that book
+	// Else, its rejected with a 404 as there is no book by that ID
+	if len(getBookDetails.ID) > 0 {
+		queryToDeleteExistingBook := `DELETE FROM BOOKMANAGEMENT WHERE ID=$1;`
+		db.QueryRow(queryToDeleteExistingBook, deleteBookDetailsParameters.BookID)
+		c.JSON(200, gin.H{"status": "Book with ID, " + deleteBookDetailsParameters.BookID + " deleted."})
+
+	} else {
+		c.JSON(404, gin.H{"status": "No Book by ID, " + deleteBookDetailsParameters.BookID + " exists."})
+	}
 }
